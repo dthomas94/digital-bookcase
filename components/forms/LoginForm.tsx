ecfbd08909dc831d53dde3131ea051b8aa83e413
dthomas94 <danielle.thomas313@gmail.com>
2023-07-30 16:12:43 -0400

    styles login screen/form
    + adds styled components
    + adds expo vector icons

--- components/forms/LoginForm.tsx
+++ components/forms/LoginForm.tsx
@@ -1,29 +1,49 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, TextInput, View } from "react-native";
+import { View, Text } from "react-native";
+import styled from "styled-components/native";
+import { AntDesign } from "@expo/vector-icons";
+
+const StyledView = styled.View`
+  align-items: "center";
+  justify-content: "center";
+  align-content: "center";
+  width: 100%;
+`;
+
+const StyledTextInput = styled.TextInput`
+  border-radius: 5px;
+  height: 40px;
+  margin-bottom: 12px;
+  border: 1px solid lightgray;
+  width: 100%;
+  padding: 10px;
+`;
+
+const StyledButton = styled.Pressable`
+  background-color: rgb(254, 167, 64);
+  border-radius: 45px;
+  padding: 15px 25px;
+  box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  column-gap: 10px;
+  color: white;
+  margin-top: 10px;
+`;
 
 export const LoginForm = () => {
   return (
-    <View style={styles.container}>
-      <TextInput placeholder="Email Address" style={styles.input} />
-      <TextInput secureTextEntry placeholder="Password" style={styles.input} />
-      <Button title="LOGIN" onPress={() => {}} accessibilityLabel="Login" />
+    <StyledView>
+      <StyledTextInput placeholder="Email Address" />
+      <StyledTextInput secureTextEntry placeholder="Password" />
+      <View style={{ alignSelf: "flex-end", marginTop: 10 }}>
+        <StyledButton onPress={() => {}} accessibilityLabel="Login">
+          <Text style={{ color: "white" }}>LOGIN</Text>
+          <AntDesign name="arrowright" size={20} color="white" />
+        </StyledButton>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </StyledView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    justifyContent: "center",
-    alignContent: "center",
-    width: "100%",
-  },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    width: " 100%",
-    padding: 10,
-  },
-});
--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,7 +1,35 @@
-import { Text, StyleSheet, View } from "react-native";
+import { Text, View } from "react-native";
 import { LoginForm } from "../components/forms/LoginForm";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../App";
+import { styled } from "styled-components/native";
+
+const StyledView = styled.View`
+  flex: 1;
+  background-color: #fff;
+  justify-content: center;
+  row-gap: 50px;
+  padding-right: 20px;
+  padding-left: 20px;
+`;
+
+const StyledHeading = styled.Text`
+  font-weight: bold;
+  font-size: 36px;
+  text-align: left;
+  margin-bottom: 10px;
+`;
+
+const StyledSubheading = styled.Text`
+  color: rgb(145, 145, 145);
+  font-weight: bold;
+`;
+
+const StyledSignupText = styled.Text`
+  align-self: center;
+  position: absolute;
+  bottom: 50px;
+`;
 
 export type LoginScreenProps = NativeStackScreenProps<
   RootStackParamList,
@@ -9,32 +37,17 @@ export type LoginScreenProps = NativeStackScreenProps<
 >;
 
 export const LoginScreen = ({ route, navigation }: LoginScreenProps) => (
-  <View style={styles.container}>
+  <StyledView>
     <View>
-      <Text style={styles.heading}>Login</Text>
-      <Text style={styles.subheading}>Please sign in to continue.</Text>
+      <StyledHeading>Login</StyledHeading>
+      <StyledSubheading>Please sign in to continue.</StyledSubheading>
     </View>
     <LoginForm />
-  </View>
+    <StyledSignupText>
+      Don't have an account?{" "}
+      <Text onPress={() => {}} style={{ color: "rgb(255, 164, 61)" }}>
+        Sign Up
+      </Text>
+    </StyledSignupText>
+  </StyledView>
 );
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-    rowGap: 50,
-    paddingRight: 20,
-    paddingLeft: 20,
-  },
-  heading: {
-    fontWeight: "bold",
-    fontSize: 36,
-    textAlign: "left",
-    marginBottom: 10,
-  },
-  subheading: {
-    color: "rgb(145, 145, 145)",
-    fontWeight: "bold",
-  },
-});
