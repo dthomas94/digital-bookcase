96f0e65791ec83ec938015f60264fdc1b8e5c3e7
dthomas94 <danielle.thomas313@gmail.com>
2023-08-01 19:38:28 -0400

    store token in local storage

--- api/users.ts
+++ api/users.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { REACT_APP_DEV_API } from "@env";
 
 const url = process.env.NODE_ENV === "development" ? REACT_APP_DEV_API : "";
@@ -23,6 +24,26 @@ export const login = async (email: string, password: string) => {
       email,
       password,
     });
+
+    const token = await AsyncStorage.getItem("authToken");
+    if (token) {
+      console.log("User is logged in");
+    } else {
+      await AsyncStorage.setItem("authToken", res.headers.authorization);
+    }
+
+    return res.data.user;
+  } catch (err: any) {
+    // janky way of getting error message because of Devise on the backend
+    console.error(err.response.data.status.message);
+  }
+};
+
+export const logout = async () => {
+  try {
+    const res = await axios.delete(`${url}/logout`);
+
+    await AsyncStorage.setItem("authToken", res.headers.authorization);
     return res.data.user;
   } catch (err: any) {
     // janky way of getting error message because of Devise on the backend
